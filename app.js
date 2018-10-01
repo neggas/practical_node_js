27fe48ceff5d44ed18ad3601845f7d1b88d950d6
Rodrigo Dominguez <rorra@rorra.com.ar>
2018-10-01 03:04:51 -0300

    Chapter 6

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const collections = {
   users: db.collection('users')
 }
 
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const logger = require('morgan');
 const errorHandler = require('errorhandler');
 const bodyParser = require('body-parser');
@@ -26,29 +28,55 @@ app.use((req, res, next) => {
   return next();
 });
 
+// Express.js configurations
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Express.js middleware configuration
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser('3CCC4ACD-6ED1-4844-9217-82131BDCB239'))
+app.use(session({secret: '2C44774A-D649-4D44-9535-46E296EF984F',
+  resave: true,
+  saveUninitialized: true}))
+
+// Authentication middleware
+app.use((req, res, next) => {
+  if (req.session && req.session.admin) {
+    res.locals.admin = true;
+  }
+  next();
+});
+
+// Authorization middleware
+const authorize = (req, res, next) => {
+  if (req.session && req.session.admin)
+    return next();
+  else
+    return res.status(401).send();
+}
 
 if (app.get('env') === 'development') {
   app.use(errorHandler('dev'));
 }
 
+// Pages & Routes
 app.get('/', routes.index);
 app.get('/login', routes.user.login);
 app.post('/login', routes.user.authenticate);
 app.get('/logout', routes.user.logout);
-app.get('/admin', routes.article.admin);
-app.get('/post', routes.article.post);
-app.post('/post', routes.article.postArticle);
+app.get('/admin', authorize, routes.article.admin);
+app.get('/post', authorize, routes.article.post);
+app.post('/post', authorize, routes.article.postArticle);
 app.get('/articles/:slug', routes.article.show);
+
+// Rest API Routes
+app.all('/api', authorize);
 app.get('/api/articles', routes.article.list);
 app.post('/api/articles', routes.article.add);
 app.put('/api/articles/:id', routes.article.edit);
--- routes/user.js
+++ routes/user.js
@@ -16,6 +16,7 @@ exports.login = (req, res, next) => {
  * GET logout route.
  */
 exports.logout = (req, res, next) => {
+  req.session.destroy();
   res.redirect('/');
 }
 
@@ -23,5 +24,19 @@ exports.logout = (req, res, next) => {
  * POST authenticate route.
  */
 exports.authenticate = (req, res, next) => {
-  res.redirect('/admin');
+  if (!req.body.email || !req.body.password) {
+    return res.render('login', {
+      error: 'Please enter your email and password.'
+    });
+  }
+  req.collections.users.findOne({
+    email: req.body.email,
+    password: req.body.password
+  }, (error, user) => {
+    if (error) return next(error);
+    if (!user) return res.render('login', {error: 'Incorrect email&password combination.'});
+    req.session.user = user;
+    req.session.admin = user.admin;
+    res.redirect('/admin');
+  });
 }
